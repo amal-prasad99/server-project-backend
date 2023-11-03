11629870aa68e07906fb3fe250f61b57ba1af034
FrostCafe <aps.project007@gmail.com>
2023-11-03 19:58:30 +0530

    first commit

---
+++ src/main/java/com/example/server/model/Server.java
@@ -0,0 +1,25 @@
+package com.example.server.model;
+
+
+import com.example.server.enumaration.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Server {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address cannot be empty or null")
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String imageUrl;
+    private Status status;
+}
